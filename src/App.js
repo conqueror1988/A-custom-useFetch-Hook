a56c134051d2f4e615730d0c7bb37f3d60ec48bd
conqueror1988 <fatih.aksoy1988@gmail.com>
2023-08-10 09:51:29 +0200

    finished

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import useFetch from "./useFetch";
+
+function App() {
+  const {data,loading,error,refetch} = useFetch("https://v2.jokeapi.dev/joke/Any");
+
+
+  if(loading) return <h1>LOADING...</h1>
+
+  if(error) console.log(error)
+
+  return (
+    <div className="App">
+      <h1>{data?.setup}:{data?.delivery}</h1>
+      <button onClick={refetch}>Refetch</button>
+    </div>
+
+  );
+}
+
+export default App;
